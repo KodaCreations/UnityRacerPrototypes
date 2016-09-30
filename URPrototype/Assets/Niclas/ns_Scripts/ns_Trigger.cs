d4318eff77a7ad843f1aef176df4fefb59deb792
Niclass <niclas.soder@gmail.com>
2016-09-30 15:10:37 +0200

    Enkel trigger till dynamisk event

---
+++ URPrototype/Assets/Niclas/ns_Scripts/ns_Trigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ns_Trigger : MonoBehaviour {
+
+    public GameObject rightDoor;
+    public GameObject leftDoor;
+    public float timerMax = 5.0f;
+    public float timer = 0.0f;
+
+    public void Update()
+    {
+        if (timer >= 0)
+            timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            rightDoor.SetActive(false);
+            leftDoor.SetActive(true);
+        }
+        else
+        {
+            rightDoor.SetActive(true);
+            leftDoor.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter()
+    {
+        if (timer <= 0)
+        {
+            timer = timerMax;
+        }
+    }
+}
