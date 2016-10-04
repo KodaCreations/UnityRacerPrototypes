7f9fe19490da3be6854d268284f553f7103acf09
M09K3730 <david90dahl@yahoo.se>
2016-10-04 20:31:55 +0200

    Created split-screen prototype

---
+++ URPrototype/Assets/David/Arrows.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrows : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("up"))
+            transform.position += transform.forward * Time.deltaTime * 20;
+        else if (Input.GetKey("down"))
+            transform.position += -transform.forward * Time.deltaTime * 10;
+        if (Input.GetKey("left"))
+            transform.Rotate(transform.up, -50 * Time.deltaTime);
+        if (Input.GetKey("right"))
+            transform.Rotate(transform.up, 50 * Time.deltaTime);
+    }
+}
---
+++ URPrototype/Assets/David/WASD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WASD : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("w"))
+            transform.position += transform.forward * Time.deltaTime * 20;
+        else if (Input.GetKey("s"))
+            transform.position += -transform.forward * Time.deltaTime * 10;
+        if (Input.GetKey("a"))
+            transform.Rotate(transform.up, -50 * Time.deltaTime);
+        if (Input.GetKey("d"))
+            transform.Rotate(transform.up, 50 * Time.deltaTime);
+    }
+}
